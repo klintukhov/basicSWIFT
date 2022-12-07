8691763960f98188ee7d957786d9ebe496f1ef73
klintukhov <gyrzov86@gmail.com>
2022-12-07 13:58:34 +0100

    lesson 3 practice

--- basicSwift.playground/Contents.swift
+++ basicSwift.playground/Contents.swift
@@ -1,12 +1,11 @@
 import UIKit
 
+var str = "Hello, playground"
+
 
 // ARRAYS
-
-
 let constArray = ["a", "b", "c", "d"]
 //                 0    1    2    3     ====>  indecsation of arrays. beginning from "0"
-
 constArray.count
 
 var array = [String]()
@@ -34,7 +33,6 @@ array
 
 
 // practice part 2
-
 var deutschAuto: [String] = ["audi", "bmw", "mercedes", "porsche", "opel", "ford", "skoda"]
 //                              0       1        2           3         4       5       6   ====>  indeks
 let tttttttt = deutschAuto [2]
@@ -64,7 +62,6 @@ deutschAuto.count
 japanAuto.count
 japanAuto.endIndex // indeks poslednego znacheniya
 japanAuto.isEmpty // proverka na pustotu
-
 print (deutschAuto )
 let chuffleDeutschAuto = deutschAuto.shuffled() // creating new value for main array
 deutschAuto.shuffle() // changing main array
@@ -72,7 +69,6 @@ print (deutschAuto)
 
 
 // filtrating of array
-
 var filterDeutschAutoFirst = deutschAuto.filter { deutschAuto   in // filtration array on first letter "b"
     deutschAuto.first == "b"
 }
@@ -83,3 +79,100 @@ let filterDeutschAutoLast = deutschAuto.filter { deutschAuto   in // filtration 
 }
 
 print (filterDeutschAutoLast)
+/*
+// practice 2
+ - создать массив "дни в месяцах"
+-элементов содержащих количество дней в соответствующем месяце
+используя цикл for и этот массив
+- выведите количество дней в каждом месяце (без имен месяцев)
+- используйте еще один массив с именами месяцев чтобы вывести название месяца + количество дней
+- сделайте тоже самое, но используя массив тюплов с параметрами (имя месяца, кол-во дней)
+- сделайте тоже самое, только выводите дни в обратном порядке (порядок в массиве не меняется)
+- для произвольно выбранной даты (месяц и день) посчитайте количество дней до этой даты от начала года
+4) Создайте массив оценок от 0 до 5 и второй — от 6 до 10. Объедените два массива в один.
+5) Присвойте каждому ученику оценку. Так, чтобы в консоли получилось примерно: «Вася — 5 баллов.Неплохо.».
+6) Создайте массив из чисел. Числа - это купюры в долларах. Посчитать количество денег и вывести в консоль.
+*/
+
+// 1) Создайте массив учеников из 12 мальчиков
+var boysArray: [String] = ["Ihor", "Oleh", "Zhenia", "Vasya", "Andrey", "Sasha", "Misha", "Vitalii", "Ehor", "Denys", "Illia",]
+
+
+//  2) Добавьте 7 девочек: с помощью append и по определённому индексу: см. insert. Выведите результат в консоль.
+var clas = boysArray
+
+clas.append("Zhanna")
+clas.append("Iryna")
+clas.append("Sveta")
+clas.append("Olga")
+clas.insert("Lena", at: 2)
+clas.insert("Vika", at: 4)
+clas.insert("Nastya", at: 2)
+
+// 3) Удалите 7 учеников так, чтобы получилось 5 мальчиков и 5 девочек. См. Удаление по диапазону. Выведите результат в консоль.
+for _ in clas [1...3] {
+    remove
+}
+
+
+
+// 4) Создайте массив оценок от 0 до 5 и второй — от 6 до 10. Объедените два массива в один.
+
+var marksLow = [0, 1, 2, 3, 4, 5]
+var marksHigh = [6, 7, 8, 9, 10,]
+var marksAll = marksLow + marksHigh  // 1y sposob
+marksLow += marksHigh                // 2y sposob
+
+  
+// 5) Присвойте каждому ученику оценку. Так, чтобы в консоли получилось примерно: «Вася — 5 баллов.Неплохо.».
+
+
+
+
+
+
+var markOne = "\(boysArray[1]), your mark is  \(marksHigh[2]), not bad)"
+var markTwo = "\(boysArray[3]), your mark is  \(marksHigh[4]), not bad)"
+var markThree = "\(boysArray[4]), your mark is  \(marksLow[3]), its not good, sorry)"
+
+// 6) Создайте массив из чисел. Числа - это купюры в долларах. Посчитать количество денег и вывести в консоль.
+var dollars = [5, 10, 10, 50, 100, 50 , 100, 100]
+var sum = 0
+for i in dollars {
+    print ("i = (\(i)")
+    sum += 1
+}
+ 
+var shoppingList = ["eggs", "milk"]
+print ("The shopping list is \(shoppingList.count)")
+if shoppingList.isEmpty {
+    print ("the shopping list is empty")
+} else {
+    print ("not empty")
+}
+
+shoppingList.append("flour")
+
+shoppingList += ["baking powder", ""]
+shoppingList += ["Chocolate Spread", "Cheese", "Butter"]
+
+var firstItem = shoppingList[0]
+shoppingList[0] = "six eggs"
+shoppingList
+shoppingList[4...6] = ["bananas", "apples"]
+shoppingList
+shoppingList.insert("maple syrop", at: 0)
+shoppingList.remove(at: 0)
+
+for index in shoppingList {
+print (index)
+}
+for (index , value) in shoppingList.enumerated() {
+    print ("Item \(index + 1) : \(value)")
+}
+
+
+let numberOfLegs = ["spider": 8, "ant": 6, "cat": 4]
+for (animalName, legCount) in numberOfLegs {
+    print("\(animalName)s have \(legCount) legs")
+}
